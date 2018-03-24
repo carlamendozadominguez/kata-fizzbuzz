d2e291078fc6e7d66f2117ad1813831db6415062
raulanatol <raul@natol.es>
2018-03-24 13:35:15 +0000

    Creación de la función

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export const fizzbuzz = (value) => {
+  return value;
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,5 @@
+import { fizzbuzz } from "../src";
+
 describe('Index', () => {
   test('Debería devolver un number como un string', () => {
     expect(fizzbuzz(1)).toBe('1');
