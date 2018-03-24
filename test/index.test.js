19ae58e4e6096cdabce42e89645b2cdede931cd7
raulanatol <raul@natol.es>
2018-03-24 13:39:18 +0000

    Test primer requisito en rojo

--- test/index.test.js
+++ test/index.test.js
@@ -4,4 +4,8 @@ describe('Index', () => {
   test('Debería devolver un number como un string', () => {
     expect(fizzbuzz(1)).toBe('1');
   });
+
+  test('Debería return FIZZ si number es divisible por 3', () => {
+    expect(fizzbuzz(6)).toBe('FIZZ');
+  });
 });
